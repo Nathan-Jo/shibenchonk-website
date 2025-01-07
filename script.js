1bde09f9af0fd92dabfae9f77d7dac34970fed57
Nathan-Jo <clubbaper62@gmail.com>
2025-01-07 20:39:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("learnMoreBtn").addEventListener("click", function () {
+    alert("Thank you for your interest in Shibenchonk Coin! Visit our website for more updates.");
+});
